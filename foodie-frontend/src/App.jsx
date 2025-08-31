8e858452ecb4761bc211ab38fa889b3e4ef5b673
Anirban <anirban140963@gmail.com>
2025-09-01 01:33:11 +0530

    Added signup page

--- foodie-frontend/src/App.jsx
+++ foodie-frontend/src/App.jsx
@@ -1,35 +1,79 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    name: "",
+    email:"",
+    password:""
+  });
+
+
+  const handelChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.password) {
+      alert("All fields are required");
+      return;
+    }
+
+    console.log("From Data Submitted:", formData);
+    alert("signup successful!");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='flex justify-center items-center h-screen bg-gray-100'>
+      <form
+       
+       onSubmit={handelSubmit}
+       className='bg-white p-8 rounded-2xl shadow-lg w-96'>
+        <h2 className='text-2xl font-bold mb-6 text-center'>Sign Up</h2>
+          
+        <input
+         type='text'
+         name='name'
+         placeholder='Full Name'
+         value={formData.name}
+         onChange={handelChange}
+         className='w-full p-2 mb-4 border rounded-lg' />
+        
+        <input 
+         type='email'
+         name='email'
+         placeholder='Email Address'
+         value={formData.email}
+         onChange={handelChange}
+         className='w-full p-2 mb-4 border rounded-lg'
+        />
+ 
+         <input 
+         type='password'
+         name='password'
+         placeholder='password'
+         value={formData.password}
+         onChange={handelChange}
+         className='w-full p-2 mb-4 border rounded-lg'
+        />
+
+        <button type='submit' className='w-full bg-blue-600 text-black py-2 rounded-lg hover:bg-blue-700 transition'>Sign Up</button>
+
+     
+      </form> 
+       
+
+
+    </div>
+  );
 }
 
 export default App
